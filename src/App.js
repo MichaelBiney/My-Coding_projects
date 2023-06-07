53d2a33e2a37271384f8bfae74d5dcc28cdf8b18
MichaelBiney <92759909+MichaelBiney@users.noreply.github.com>
2023-06-07 09:15:32 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,46 +1,31 @@
-import React, { useState} from "react";
-import Expenses from "./componets/Expenses/Expenses";
-import NewExpense from "./componets/NewExpense/NewExpense";
+import React, { useState } from "react";
+import Header from "./componets/Layout/Header";
+import Meals from './componets/Meals/Meals';
+import Cart from "./componets/Cart/Cart";
+import Cartprovider from "./store/CartProvider";
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2023, 1, 14),
-  },
-  { id: 'e2', 
-  title: 'New TV', 
-  amount: 799.49, 
-  date: new Date(2021, 2, 12)},
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2022, 3, 12),
-  },
-];
+function App() {
+  const  [cartIshown, setCartIsShown] = useState(false); 
 
-const App = () =>{
-  const  [expenses, SetExpenses] = useState(DUMMY_EXPENSES)
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-    const addExpenseHandler = expense => {
-      SetExpenses((prevExpenses) =>{
-        return [ expense, ...prevExpenses];
-      });
-    };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
 
-  return(
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses  items={expenses}/>
-    </div>
-    );
+  };
+
+
+  return (
+    <Cartprovider>
+      {cartIshown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
+      <main>
+    <Meals/>
+      </main>
+    </Cartprovider>
+  );
 }
+
 export default App;
